b582b73bcb362edec44ad33a3af8a4081b8b3cc9
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-12-13 16:51:34 +0400

    liebe

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,30 @@
+// import { Header } from '../../../components/header'
+// import { Footer } from '../../../components/footer'
+// import { BlogPost } from '../../../components/blog-post'
+
+// export default function BlogPostPage({ params }: { params: { id: string } }) {
+//   return (
+//     <div className="min-h-screen bg-background flex flex-col">
+//       <Header />
+//       <main className="flex-grow container mx-auto px-4 py-8">
+//         <BlogPost id={params.id} />
+//       </main>
+//       <Footer />
+//     </div>
+//   )
+// }
+
+
+
 import { Header } from '../../../components/header'
 import { Footer } from '../../../components/footer'
 import { BlogPost } from '../../../components/blog-post'
 
-export default function BlogPostPage({ params }: { params: { id: string } }) {
+interface BlogPostPageProps {
+  params: { id: string }
+}
+
+export default function BlogPostPage({ params }: BlogPostPageProps) {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Header />
